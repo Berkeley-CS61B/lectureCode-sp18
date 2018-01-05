fc50fc95344dd91871c79ffaa5df7ade7a543865
Josh Hug <hug@cs.berkeley.edu>
2018-01-05 13:30:51 -0800

    added lecture code for lecture 1

---
+++ intro1/hellonumbers.py
@@ -0,0 +1,9 @@
+x = 0
+while x < 10:
+	print(x)
+	x = x + 1
+
+x = "horse"
+print(x)
+
+print(5 + "horse")
---
+++ intro1/larger.py
@@ -0,0 +1,7 @@
+def larger(x, y):
+	"""Returns the larger of x and y."""
+	if (x > y):
+		return x
+	return y
+
+print(larger(-5, 10))
